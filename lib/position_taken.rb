a942a78fca4b7e9f9d10c51acb32c1ef22d932fc
Kathy Hudson <khitx@icloud.com>
2017-12-09 23:10:01 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     return false
-  elsif (board[index] == 'x' || board[index] == 'o')
+  elsif (board[index] == "x" || board[index] == "o")
     return true
   end
 end
